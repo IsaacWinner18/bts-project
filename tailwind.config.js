b72f694abf8b0f04c4d91ac255b54567f234ca1e
IsaacWinner18 <149059161+IsaacWinner18@users.noreply.github.com>
2024-02-01 23:31:18 -0800

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,12 @@ module.exports = {
     extend: {
       backgroundColor: {
         "blue-990": "rgba(0, 29, 130, 1)",
+        "red-990": "rgba(130, 0, 0, 1)",
         "shade-purple": "rgba(142, 122, 253, 0.7)",
       },
       textColor: {
         "blue-990": "rgba(0, 29, 130, 1)",
+        "red-990": "rgba(130, 0, 0, 1)",
       },
       width: {
         "30": "7.5rem",
@@ -16,10 +18,24 @@ module.exports = {
         "38": "38%",
         "42": "42%",
         "85": "85%",
+        "995": "995px",
+        "332": "332px",
+        "3.5": "34px",
       },
       height: {
         "100": "100vh",
+        "300": "253px",
       },
+      fontSize: {
+        "27": "27.39px",
+        "21": "21.04px",
+      },
+      borderRadius: {
+        "8": "8px",
+      },
+      borderWidth: {
+        "1": "1px"
+      } 
     },
   },
   plugins: [],
